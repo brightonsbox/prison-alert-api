4ffc224a453e3e7cdf15448911bf9fbbb8a1ce6c
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2020-11-28 11:42:09 +0000

    Updated PrisonApiClient class to include PrisonAlertApiConfig

--- src/test/java/uk/gov/justice/hmpps/prisonalertapi/client/prisonapi/PrisonApiClientTest.java
+++ src/test/java/uk/gov/justice/hmpps/prisonalertapi/client/prisonapi/PrisonApiClientTest.java
@@ -1,16 +1,39 @@
 package uk.gov.justice.hmpps.prisonalertapi.client.prisonapi;
 
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.reactive.function.client.WebClient;
 import uk.gov.justice.hmpps.prisonalertapi.client.prisonapi.dto.Alert;
+import uk.gov.justice.hmpps.prisonalertapi.config.PrisonAlertApiConfig;
+
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class PrisonApiClientTest {
-    private final PrisonApiClient prisonApiClient = new PrisonApiClient(null);
+    private final MockWebServer prisonApiMock = new MockWebServer();
+    private PrisonApiClient prisonApiClient;
+
+
+    @BeforeEach
+    void beforeEach() {
+        prisonApiClient = new PrisonApiClient(WebClient.create(), new PrisonAlertApiConfig(String.format("http://localhost:%s", prisonApiMock.getPort())));
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        prisonApiMock.shutdown();
+    }
 
     @Test
     void getAlert() {
-        assertThat(prisonApiClient.getAlert(1, 3)).isEqualTo(new Alert(3L));
+        prisonApiMock.enqueue(new MockResponse().setHeader("Content-type", "application/json").setBody("{\"alertId\": 3, \"alertType\": \"AA\"}"));
+        assertThat(prisonApiClient.getAlert(1, 3)).isEqualTo(new Alert(3L, "AA"));
     }
 }
